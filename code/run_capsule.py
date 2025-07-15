085c30c189b5a353d89313053158b0ffa2d85c23
alejoe91 <alessio.buccino@alleninstitute.org>
2025-07-15 07:47:06 +0000

    set mp_contect to spawn by default

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     # Use CO_CPUS/SLURM_CPUS_ON_NODE env variable if available
     N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("SLURM_CPUS_ON_NODE")
     N_JOBS = int(N_JOBS_EXT) if N_JOBS_EXT is not None else -1
-    job_kwargs = dict(n_jobs=N_JOBS, progress_bar=False)
+    job_kwargs = dict(n_jobs=N_JOBS, progress_bar=False, mp_context="spawn")
     si.set_global_job_kwargs(**job_kwargs)
 
     ecephys_sorted_folders = [
